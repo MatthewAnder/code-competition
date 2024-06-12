871f9539acee9a068167df542c37e21e93cfd172
Matthew <mharyant@students.cs.ubc.ca>
2024-06-12 12:36:51 -0700

    initialize

---
+++ src/components/Provider.tsx
@@ -0,0 +1,7 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return <ChakraProvider>{children}</ChakraProvider>;
+};
+
+export default Provider;
